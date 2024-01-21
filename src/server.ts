75b5f4792dd81db02e2099dbece1d60d1ec06395
Mario Krstevski <mariokrstevski@hotmail.com>
2024-01-21 01:04:40 +0100

    api actions

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import express from "express";
 import prismadb from "./prismadb";
 import cors from "cors";
 import { JobApplication as JobApplicationType } from "@prisma/client";
+import { EditTypes } from "./types";
 const app = express();
 const port = process.env.PORT || 5050;
 
@@ -13,7 +14,230 @@ app.use(
     origin: "*",
   })
 );
+app.patch("/archiveJobApplication", async (req, res) => {
+  const { ids, isArchived, userId } = req.body;
 
+  if (!userId) {
+    res.status(400).json({ error: "userId is required" });
+  }
+
+  if (!ids) {
+    res.status(400).json({ error: "Id is missing" });
+  }
+
+  if (
+    !Array.isArray(ids) &&
+    ids.any((id: string) => typeof id !== "string")
+  ) {
+    res
+      .status(400)
+      .json({ error: "Ids must be an array of strings" });
+  }
+
+  if (ids.length === 0) {
+    res.status(400).json({ error: "Ids array must not be empty" });
+  }
+
+  if (ids.length === 1) {
+    console.log("We are archiving one job application");
+  } else if (ids.length > 1) {
+    console.log(
+      "We are archiving multiple job applications - " + ids.length
+    );
+  }
+
+  try {
+    const archivedApplications =
+      await prismadb.jobApplication.updateMany({
+        where: {
+          id: {
+            in: ids,
+          },
+          userId: userId,
+        },
+        data: {
+          isArchived: isArchived,
+        },
+      });
+
+    console.log("archivedApplications", archivedApplications);
+
+    res.json(archivedApplications);
+  } catch (error) {
+    console.error("Error creating job application:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+app.delete("/deleteJobApplication", async (req, res) => {
+  const { ids, userId } = req.body;
+
+  if (!userId) {
+    res.status(400).json({ error: "userId is required" });
+  }
+
+  if (!ids) {
+    res.status(400).json({ error: "Id is missing" });
+  }
+
+  if (
+    !Array.isArray(ids) &&
+    ids.any((id: string) => typeof id !== "string")
+  ) {
+    res
+      .status(400)
+      .json({ error: "Ids must be an array of strings" });
+  }
+
+  if (ids.length === 0) {
+    res.status(400).json({ error: "Ids array must not be empty" });
+  }
+
+  if (ids.length === 1) {
+    console.log("We are deleting one job application");
+  } else if (ids.length > 1) {
+    console.log(
+      "We are deleting multiple job applications - " + ids.length
+    );
+  }
+
+  try {
+    const deletedApplications =
+      await prismadb.jobApplication.deleteMany({
+        where: {
+          id: {
+            in: ids,
+          },
+          userId: userId,
+        },
+      });
+
+    console.log("deletedApplications", deletedApplications);
+
+    res.json(deletedApplications);
+  } catch (error) {
+    console.error("Error creating job application:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.patch("/editJobApplication", async (req, res) => {
+  const {
+    jobApplication,
+    userId,
+    type,
+  }: {
+    jobApplication: any;
+    userId: string;
+    type: EditTypes;
+  } = req.body;
+
+  const changeType = type;
+
+  if (!userId) {
+    res.status(400).json({ error: "userId is required" });
+  }
+  if (!jobApplication) {
+    res
+      .status(400)
+      .json({ error: "Job application object is missing" });
+  }
+  if (changeType === "allChange") {
+    if (!jobApplication.status) {
+      res.status(400).json({ error: "Starting status is required" });
+    }
+
+    if (!jobApplication.jobTitle) {
+      res.status(400).json({ error: "A job title is required" });
+    }
+
+    if (!jobApplication.companyName) {
+      res.status(400).json({ error: "Company name is required" });
+    }
+  }
+
+  if (changeType === "nextInterviewDateChange") {
+    // doesnt make sence to check since nextInterviewDate can be null maybe we ant to remove this thing
+  }
+
+  if (changeType === "statusChange") {
+    if (!jobApplication.status) {
+      res.status(400).json({ error: "Starting status is required" });
+    }
+  }
+
+  console.log("jobApplication", jobApplication);
+  // console.log("reqbody", req.body);
+  // console.log("userId", userId);
+
+  // res.status(200).json({ message: "success" });
+  // return;
+
+  try {
+    const edittedApplication = await prismadb.jobApplication.update({
+      where: {
+        id: jobApplication.id,
+        userId: userId,
+      },
+      data: {
+        ...jobApplication,
+      },
+    });
+
+    console.log("edittedApplication", edittedApplication);
+
+    res.json(edittedApplication);
+  } catch (error) {
+    console.error("Error creating job application:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+app.post("/createJobApplication", async (req, res) => {
+  const { jobApplication, userId } = req.body;
+
+  console.log("jobApplication", jobApplication);
+  // console.log("reqbody", req.body);
+  console.log("userId", userId);
+
+  // res.status(200).json({ message: "success" });
+  // return;
+  if (!userId) {
+    res.status(400).json({ error: "userId is required" });
+  }
+
+  if (!jobApplication) {
+    res
+      .status(400)
+      .json({ error: "Job application object is missing" });
+  }
+
+  if (!jobApplication.status) {
+    res.status(400).json({ error: "Starting status is required" });
+  }
+
+  if (!jobApplication.jobTitle) {
+    res.status(400).json({ error: "A job title is required" });
+  }
+
+  if (!jobApplication.companyName) {
+    res.status(400).json({ error: "Company name is required" });
+  }
+
+  try {
+    const newApplication = await prismadb.jobApplication.create({
+      data: {
+        ...jobApplication,
+        userId,
+      },
+    });
+
+    console.log("newApplication", newApplication);
+
+    res.json(newApplication);
+  } catch (error) {
+    console.error("Error creating job application:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 app.post("/jobApplications", async (req, res) => {
   const { userId } = req.body;
 
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export type EditTypes =
+  | "statusChange"
+  | "nextInterviewDateChange"
+  | "allChange";
