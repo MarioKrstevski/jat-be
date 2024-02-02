84b9656648f39c781514206d7e4ae6ba3256e344
Mario Krstevski <mariokrstevski@hotmail.com>
2024-02-02 18:44:03 +0100

    remove unused stuff

--- src/schemas/applications.schema.ts
+++ src/schemas/applications.schema.ts
@@ -3,7 +3,6 @@ import { EditTypes } from "../types";
 import { validDateString } from "../utils/lib";
 export const createApplicationSchema = z.object({
   body: z.object({
-    // userId: z.string().min(1, { message: "userId is required" }),
     application: z.object(
       {
         status: z
@@ -31,23 +30,13 @@ export const createApplicationSchema = z.object({
   }),
 });
 
-// export const getApplicationsSchema = z.object({
-//   query: z.object({
-//     userId: z.string().min(1, { message: "userId is required" }),
-//   }),
-// });
-
 export const getApplicationSchema = z.object({
   query: z.object({
-    // userId: z.string().min(1, { message: "userId is required" }),
     applicationId: z.string(),
   }),
 });
 export const archiveApplicationsSchema = z.object({
   body: z.object({
-    // userId: z
-    //   .string({ required_error: "userId is required" })
-    //   .min(1, { message: "Cannot be an empty string" }),
     ids: z
       .array(z.string(), { required_error: "Ids array is required" })
       .nonempty({ message: "Ids array must not be empty" }),
@@ -63,7 +52,6 @@ const EditTypes = z.enum([
 export const initialEditValidationSchema = z.object({
   body: z.object({
     type: EditTypes,
-    // userId: z.string().min(1, { message: "userId is required" }),
   }),
   params: z.object({
     applicationId: z.string(),
@@ -84,14 +72,14 @@ export const allChangeSchema = z.object({
         })
         .min(1, {
           message: "jobTitle must have at least one character",
-        }), // Assuming jobTitle is also required
+        }),
       companyName: z
         .string({
           required_error: "companyName is required",
         })
         .min(1, {
           message: "companyName must have at least one character",
-        }), // Assuming companyName is also required
+        }),
     }),
   }),
 });
@@ -137,9 +125,6 @@ export const editSchemaDecider = (type: EditTypes) => {
 
 export const deleteApplicationsSchema = z.object({
   body: z.object({
-    // userId: z
-    //   .string({ required_error: "userId is required" })
-    //   .min(1, { message: "Cannot be an empty string" }),
     ids: z
       .array(z.string(), { required_error: "Ids array is required" })
       .nonempty({ message: "Ids array must not be empty" }),
